569f7b114fdfa58c2bff880e8a12699e724dfe90
Matthew <145388707+DarkCheese63@users.noreply.github.com>
2025-12-02 16:42:26 -0600

    Delete proj directory

--- proj/riscv/test_branch.s
+++
@@ -1,8 +0,0 @@
-addi x1, x0, 1
-addi x2, x0, 1
-beq  x1, x2, TARGET  # Branch Taken!
-addi x3, x0, 5       # Should be FLUSHED
-addi x4, x0, 6       # Should be FLUSHED
-TARGET:
-addi x5, x0, 7       # Should execute
-wfi
--- proj/riscv/test_jal.s
+++
@@ -1,5 +0,0 @@
-jal  x1, TARGET    # x1 should become PC+4
-addi x2, x0, 0     # Flushed
-TARGET:
-addi x3, x0, 5
-wfi
